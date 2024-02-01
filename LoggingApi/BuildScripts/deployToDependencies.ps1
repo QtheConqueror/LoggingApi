a8821b3879658f15a27914afd631dcb449a942e4
QtheConqueror <420q69@gmail.com>
2024-01-31 18:06:03 -0700

    adjust build scripts

--- LoggingApi/BuildScripts/deployToDependencies.ps1
+++ LoggingApi/BuildScripts/deployToDependencies.ps1
@@ -1,5 +1,5 @@
 param ($OutDir, $TargetName, $ProjectName)
 echo "Coppied $TargetName.dll -> ..\..\Dependencies\$ProjectName.dll"
-Copy-Item -Path "$OutDir\$TargetName.dll" -Destination "..\..\Dependencies\$ProjectName.dll"
+Copy-Item -Path "$OutDir\$TargetName.dll" -Destination "..\..\..\Dependencies\$ProjectName.dll"
 echo "Coppied $TargetName.xml -> ..\..\Dependencies\$ProjectName.xml"
-Copy-Item -Path "$OutDir\$TargetName.xml" -Destination "..\..\Dependencies\$ProjectName.xml"
+Copy-Item -Path "$OutDir\$TargetName.xml" -Destination "..\..\..\Dependencies\$ProjectName.xml"
